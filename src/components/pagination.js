1e41f75c388b16f5edeba3914655cc3c7889bc19
Marat <maratmuhametgaliev1@gmail.com>
2026-07-19 21:00:31 +0500

    first

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,18 +1,39 @@
-import {getPages} from "../lib/utils.js";
+import { getPages } from "../lib/utils.js";
 
-export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) => {
-    // @todo: #2.3 — подготовить шаблон кнопки для страницы и очистить контейнер
+export const initPagination = ({ pages, fromRow, toRow, totalRows }, createPage) => {
+    const pageTemplate = pages.firstElementChild.cloneNode(true);
+    pages.firstElementChild.remove();
+    let pageCount;
 
-    return (data, state, action) => {
-        // @todo: #2.1 — посчитать количество страниц, объявить переменные и константы
-
-        // @todo: #2.6 — обработать действия
-
-        // @todo: #2.4 — получить список видимых страниц и вывести их
-
-        // @todo: #2.5 — обновить статус пагинации
-
-        // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
-        return data.slice(0, 10);
+    const applyPagination = (query, state, action) => {
+        const limit = state.rowsPerPage;
+        let page = state.page;
+        if (action) switch (action.name) {
+            case 'prev': page = Math.max(1, page - 1); break;
+            case 'next': page = Math.min(pageCount, page + 1); break;
+            case 'first': page = 1; break;
+            case 'last': page = pageCount; break;
+        }
+        return Object.assign({}, query, {
+            limit,
+            page
+        });
     }
+
+    const updatePagination = (total, { page, limit }) => {
+        pageCount = Math.ceil(total / limit);
+        const visiblePages = getPages(page, pageCount, 5);
+        pages.replaceChildren(...visiblePages.map(pageNumber => {
+            const el = pageTemplate.cloneNode(true);
+            return createPage(el, pageNumber, pageNumber === page);
+        }));
+        fromRow.textContent = (page - 1) * limit + 1;
+        toRow.textContent = Math.min((page * limit), total);
+        totalRows.textContent = total;
+    }
+
+    return {
+        updatePagination,
+        applyPagination
+    };
 }
--- src/main.js
+++ src/main.js
@@ -6,12 +6,15 @@ import {data as sourceData} from "./data/dataset_1.js";
 import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
 
-import {initTable} from "./components/table.js";
 // @todo: подключение
-
+import { initTable } from "./components/table.js";
+import { initPagination } from "./components/pagination.js";
+import { initSorting } from "./components/sorting.js";
+import { initFiltering } from "./components/filtering.js";
+import { initSearching } from "./components/searching.js";
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const api = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
@@ -19,9 +22,13 @@ const {data, ...indexes} = initData(sourceData);
  */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
 
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page,
     };
 }
 
@@ -29,26 +36,55 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
+async function render(action) {
     let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
+    let query = {}; // копируем для последующего изменения
     // @todo: использование
-
-
-    sampleTable.render(result)
+    query = applySeraching(query, state, action);
+    query = applyFiltering(query, state, action);
+    query = applySorting(query, state, action);
+    query = applyPagination(query, state, action);
+    const { total, items } = await api.getRecords(query);
+    updatePagination(total, query);
+    sampleTable.render(items);
 }
 
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
-    after: []
+    before: ['search', 'header', 'filter'],
+    after: ['pagination']
 }, render);
 
 // @todo: инициализация
 
+const { applyPagination, updatePagination } = initPagination(
+    sampleTable.pagination.elements,
+    (el, page, isCurrent) => {
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+);
 
+const applySorting = initSorting([
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
+
+const { applyFiltering, updateIndexes } = initFiltering(sampleTable.filter.elements);
+const applySeraching = initSearching('search');
 const appRoot = document.querySelector('#app');
+
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+    const indexes = await api.getIndexes();
+
+    updateIndexes(sampleTable.filter.elements, { searchBySeller: indexes.sellers });
+};
+
+init().then(render);
